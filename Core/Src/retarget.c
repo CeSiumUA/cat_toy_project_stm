eb75718207716fd6fd3c5b03236f4ef754abb160
CeSium <fedir.katushonok@gmail.com>
2024-07-24 08:00:35 +0300

    chore: Initialize print_uart variable to NULL in retarget.c

--- Core/Src/retarget.c
+++ Core/Src/retarget.c
@@ -1,6 +1,6 @@
 #include "retarget.h"
 
-static UART_HandleTypeDef *print_uart;
+static UART_HandleTypeDef *print_uart = NULL;
 
 void retarget_init(UART_HandleTypeDef *huart)
 {
@@ -9,6 +9,9 @@ void retarget_init(UART_HandleTypeDef *huart)
 
 int _write(int file, char *ptr, int len)
 {
+    if (print_uart == NULL) {
+        return -1;
+    }
     HAL_UART_Transmit(print_uart, (uint8_t *)ptr, len, 0xFFFF);
     return len;
 }
