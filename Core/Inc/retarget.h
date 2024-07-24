f8a370ca3262ad7c5599c150fb6c6e7ab0e1bcb3
CeSium <fedir.katushonok@gmail.com>
2024-07-24 20:40:11 +0300

    Added read functionality

--- Core/Inc/retarget.h
+++ Core/Inc/retarget.h
@@ -1,9 +1,16 @@
 #ifndef INC_RETARGET_H_
 #define INC_RETARGET_H_
 
+#include <errno.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include "stm32c0xx_hal.h"
+
+#define STDIN_FILENO           0
+#define STDOUT_FILENO          1
+#define STDERR_FILENO          2
 
 void retarget_init(UART_HandleTypeDef *huart);
 
--- Core/Src/retarget.c
+++ Core/Src/retarget.c
@@ -2,16 +2,41 @@
 
 static UART_HandleTypeDef *print_uart = NULL;
 
-void retarget_init(UART_HandleTypeDef *huart)
-{
+void retarget_init(UART_HandleTypeDef *huart) {
     print_uart = huart;
+    setvbuf(stdout, NULL, _IONBF, 0);
 }
 
-int _write(int file, char *ptr, int len)
-{
+int _write(int file, char *ptr, int len) {
+    HAL_StatusTypeDef status = HAL_OK;
     if (print_uart == NULL) {
         return -1;
     }
-    HAL_UART_Transmit(print_uart, (uint8_t *)ptr, len, 0xFFFF);
+
+    if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+        status = HAL_UART_Transmit(print_uart, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+        if (status != HAL_OK) {
+            return EIO;
+        }
+    } else{
+        errno = EBADF;
+        return -1;
+    }
+
     return len;
+}
+
+int _read(int file, char *ptr, int len){
+    HAL_StatusTypeDef status = HAL_OK;
+
+    if(file == STDIN_FILENO){
+        status = HAL_UART_Receive(print_uart, (uint8_t *)ptr, 1, HAL_MAX_DELAY);
+        if (status != HAL_OK) {
+            return EIO;
+        }
+        return 1;
+    } else {
+        errno = EBADF;
+        return -1;
+    }
 }
